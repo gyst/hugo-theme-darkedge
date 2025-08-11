7e7ce8e4b8310332ffb9b384f2320996e8e76935
Guido A.J. Stevens <guido.stevens@cosent.nl>
2025-08-11 14:55:55 +0200

    Support automatic dark mode (except graph)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: 'class',
+  darkMode: 'media',
   content: ['./layouts/**/*.html', './assets/ts/**/*.ts'],
   plugins: [require('@tailwindcss/typography')],
   safelist: ['chroma']
